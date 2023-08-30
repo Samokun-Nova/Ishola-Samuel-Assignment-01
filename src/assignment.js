06ca54362540fbb69c2b2403005032a0f2f464c3
Ishola Samuel <mokunnova@gmail.com>
2023-08-30 21:44:27 +0000

    Attempt Challenge 1 and 3

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,14 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
+    for(let i = 1; i <= destination; i++) {
+        sum += i
+    }
     return sum
 }
 
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+    assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -68,11 +71,15 @@ function countEvenNumbersWithin(destination) {
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
     let result = [];
-
+    for(let i = 0; i < arrayOfNumbers.length; i++) {
+        let Fahrenheit = (arrayOfNumbers[i] * 9/5) + 32;
+        Fahrenheit = Math.round(Fahrenheit);
+        result.push(Fahrenheit)
+    }
     return result;
 }
 
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+    assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 // ========================
 // DO NOT EDIT THIS BLOCK
